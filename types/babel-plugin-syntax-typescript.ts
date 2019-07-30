d7ab2cbe3879b6ba60b2a03de6f9ed1b995689b7
Yugo Yagita <2000ygkn0713@gmail.com>
2019-07-30 23:17:35 +0900

    add type difinition

---
+++ types/babel-plugin-syntax-typescript.ts
@@ -0,0 +1,7 @@
+declare module "@babel/plugin-syntax-typescript" {
+  function syntaxTypeScript(): {
+    manipulateOptions(opts: any, parserOpts: { plugins: string[] }): void;
+  };
+
+  export default syntaxTypeScript;
+}
