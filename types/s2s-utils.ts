208f3411630b170feb0ee91be0a9c76e1d74694a
Yugo Yagita <2000ygkn0713@gmail.com>
2019-08-06 19:15:28 +0900

    fix s2s-utils type defenition

--- types/s2s-utils.ts
+++ types/s2s-utils.ts
@@ -1,5 +1,11 @@
 declare module 's2s-utils' {
   import babelTemplate from '@babel/template';
+  import * as types from '@babel/types';
 
   export function template(code: string, plugins?: string[]): ReturnType<typeof babelTemplate>;
+
+  export function createImportDeclaration(
+    locals: Array<string> | string,
+    source: string,
+  ): types.ImportDeclaration;
 }
